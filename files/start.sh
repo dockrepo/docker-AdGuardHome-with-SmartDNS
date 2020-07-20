1294cb778eaba935783a9eb6b46476e5499e44c8
Zyao89 <zyao89@gmail.com>
2020-07-20 20:57:20 +0800

    feat: add `env.OPEN_SMARTDNS = false` 默认关闭 smartdns

--- files/start.sh
+++ files/start.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
-if [ ! -f /opt/conf/smartdns.conf ]; then
-    cp -u /config.conf /opt/conf/smartdns.conf
+
+# open smartdns
+OPEN_SMARTDNS=${OPEN_SMARTDNS:=false}
+
+if [ $OPEN_SMARTDNS = 'true' ]; then
+    if [ ! -f /opt/conf/smartdns.conf ]; then
+        cp -u /config.conf /opt/conf/smartdns.conf
+    fi
+    exec /smartdns/sbin/smartdns -f -x -c /opt/conf/smartdns.conf &
 fi
-exec /smartdns/sbin/smartdns -f -x -c /opt/conf/smartdns.conf &
 
 # adguardhome
 exec /opt/adguardhome/AdGuardHome -h 0.0.0.0 -c /opt/conf/AdGuardHome.yaml -w /opt/conf/work --no-check-update
